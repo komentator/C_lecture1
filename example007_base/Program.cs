cbecee38098cfb3d8cc90a796ef0b52d0afe1bad
Артюшенко Александр <zasranechamozur@gmail.com>
2022-10-09 15:56:54 +0300

    Большее число

---
+++ example007_base/Program.cs
@@ -0,0 +1,13 @@
+﻿
+int a = 4;
+int b = 6;
+int c = 8;
+int d = 3;
+int e = 5;
+int max = a;
+
+if (max < b) max = b;
+if (max < c) max = c;
+if (max < d) max = d;
+if (max < e) max = e;
+Console.WriteLine(max);
