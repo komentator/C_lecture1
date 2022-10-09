412f94617bdddd8857f66c9caaad0772fdc1e955
Артюшенко Александр <zasranechamozur@gmail.com>
2022-10-09 15:44:55 +0300

    If else

---
+++ example006_helloUsername/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите имя пользователя -> ");
+string username = Console.ReadLine();
+if(username.ToLower() == "саша")  // .ToLower - перевод всех символов строки в НИЖНИЙ регисрт
+{
+    Console.WriteLine("Ух ты это же Саша");
+}
+else
+{
+    Console.Write("Привет : ");
+    Console.WriteLine(username);
+}
