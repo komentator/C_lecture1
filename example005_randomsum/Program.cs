be57e49684bb2c044367b06e48afac721526f5e6
Артюшенко Александр <zasranechamozur@gmail.com>
2022-10-09 14:45:06 +0300

    add random summ

---
+++ example005_randomsum/Program.cs
@@ -0,0 +1,9 @@
+﻿int num1 = new Random().Next(1 , 10);  // генератор случайных чисел от 1 до 9
+Console.Write("Первое число -> ");
+Console.WriteLine(num1);
+int num2 = new Random().Next(1 , 10);  // генератор случайных чисел от 1 до 9
+Console.Write("Второе число -> ");
+Console.WriteLine(num2);
+int result = num1+num2;
+Console.WriteLine("Cумма чисел ");
+Console.WriteLine(result);
